d018690850772a4afc7db92adff24e08cd6c8547
Sakunaaa <robertdaszkiewicz9@gmail.com>
2022-01-03 17:01:36 +0100

    Add params functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import { Fireworks } from "fireworks-js";
+const params = new URLSearchParams(window.location.search)
+const name = params.getAll('name')
+const span = document.querySelector("#name")
 
-const container = document.querySelector('.fireworks-container')
-const fireworks = new Fireworks(container, { })
-
-fireworks.setOptions({ delay: { min: 10, max: 15 }})
-
-const runFireworks = document.getElementById("fireworks-btn")
-runFireworks.addEventListener("click", (e) => {
-    e.preventDefault()
-    fireworks.start()
-} )
-
-const parms = new URLSearchParams(window.location.search)
-for (const param of params) {
-    console.log(param)
-  }
+span.innerText = name + "!"
